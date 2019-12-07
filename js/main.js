972ffd04f15da6b6d54d374c759cb7e5606f88aa
Volkov Slava <volkov.slava@hotmail.com>
2019-12-07 20:07:35 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.slider__list').slick({
+    arrows: true,
+    appendArrows: $('.slider__arrows'),
+    prevArrow: '<a class="slider__link"><svg class="slider__svg slider__svg--left"><use xlink:href="./img/sprite.svg#arrow"></use></svg></a>',
+    nextArrow: '<a class="slider__link"><svg class="slider__svg slider__svg--right"><use xlink:href="./img/sprite.svg#arrow"></use></svg></a>'
+    });
